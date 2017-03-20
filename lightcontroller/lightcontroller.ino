be0642db1e7386c5b3f04d6cd1263a44a86b0b31
ihopeidontforgetthisaccname <weidnersimon@gmx.de>
2017-03-20 19:38:46 +0100

    made buffer throw away old msgs

--- lightcontroller/lightcontroller.ino
+++ lightcontroller/lightcontroller.ino
@@ -1,33 +1,59 @@
 struct LED {
-  int bluepin;
   int redpin;
   int greenpin;
-
-
-
-
-
+  int bluepin;
 };
 
-LED led1 = {2,3,4}; 
+byte serial_buffer[3];
+
+LED led1 = {3,4,2}; 
 
 
 void activateLed(struct LED* led) {
   pinMode(led->redpin, OUTPUT);
-    pinMode(led->bluepin, OUTPUT);
-      pinMode(led->greenpin, OUTPUT);
-
+  pinMode(led->bluepin, OUTPUT);
+  pinMode(led->greenpin, OUTPUT);
 }
 
 void setup()
 {
  activateLed(&led1);
+ Serial.begin(9600);
+}
+
+bool read_serial_buffer(int buffer_size, byte* buffer)
+{
+  if (Serial.available() < buffer_size) {
+    return false;
+  }
+ 
+    // Read buffer
+    for (int i = 0; i < buffer_size; i++)
+    {
+      buffer[i] = Serial.read();
+      delay(10); 
+    }
+
+}
+
+void empty_serial() 
+{
+
+    while (Serial.available()) 
+    {
+      Serial.read();
+    }
+    return true;
 }
 
 void loop()
 {
-   analogWrite(led1.greenpin, 0);
-   analogWrite(led1.redpin, 0);
-   analogWrite(led1.bluepin, 200);
+  if (read_serial_buffer(3, serial_buffer))
+    empty_serial();
+ 
+   analogWrite(led1.redpin, serial_buffer[0]); 
+   analogWrite(led1.greenpin, serial_buffer[1]);
+
+   analogWrite(led1.bluepin, serial_buffer[2]);
 }
 
